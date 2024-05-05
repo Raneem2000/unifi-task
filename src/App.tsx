d4d6aee9435208a7e1f4c915a20ee5b36f74df03
Raneem2000 <rdasouki6@gmail.com>
2024-05-06 01:29:39 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Loader from './common/Loader';
+import Test from './Pages/Test';
+
+function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      <Routes>
+       <Route path='/' element={<Test/>}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Test.tsx
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import Topbar from '../components/topBar';
+import Bike from './Bikes/Bike';
+const Test = () => {
+  return (
+    <>
+      <Topbar />
+      <Bike />
+      <Footer />
+    </>
+  );
+};
+
+export default Test;
