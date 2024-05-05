93ba5d20269549e5035c6c97173c1b6b2592deec
Raneem2000 <rdasouki6@gmail.com>
2024-05-06 01:33:55 +0300

    .. commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import './css/style.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 const queryClient = new QueryClient();
 
