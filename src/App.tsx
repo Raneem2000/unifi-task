cc38d12b0602f42f3710ba43afeb0403e3513029
Raneem2000 <rdasouki6@gmail.com>
2024-05-06 01:35:31 +0300

    .. commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
-import Loader from './common/Loader';
 import Test from './Pages/Test';
 
 function App() {
@@ -15,12 +14,10 @@ function App() {
     setTimeout(() => setLoading(false), 1000);
   }, []);
 
-  return loading ? (
-    <Loader />
-  ) : (
+  return (
     <>
       <Routes>
-       <Route path='/' element={<Test/>}/>
+        <Route path="/" element={<Test />} />
       </Routes>
     </>
   );
